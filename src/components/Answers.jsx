9f6829201efbc51c5c41e5e52df7883f8893ed40
RafaelCENG <rnicolaouceng@gmail.com>
2022-11-12 13:01:43 +0200

    Added onClick on the names

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,20 +1,33 @@
-const Answers = ({ shuffleAnswers, randomCharacter }) => {
+const Answers = ({
+	shuffleAnswers,
+	randomCharacter,
+	character,
+	handleChoice,
+}) => {
+	//console.log('Character', character)
+	//console.log('shuffleAnswers', shuffleAnswers)
+	//console.log('randomCharacter', randomCharacter)
 	// console.log([...shuffleAnswers, ...randomCharacter])
 	//console.log(shuffleAnswers)
 	//console.log(randomCharacter)
-	const shuffledAnswers = shuffleAnswers
-		.concat(randomCharacter)
-		.sort(() => Math.random() - 0.5)
-		.map((answer) => ({ ...answer }))
-	//console.log(shuffledAnswers)
+	// console.log('First shuffle', shuffleAnswers)
 
+	// const shuffledAnswers = shuffleAnswers
+	// 	.concat(randomCharacter)
+	// 	.sort(() => Math.random() - 0.5)
+	// 	.map((answer) => ({ ...answer }))
+	// console.log('Shuffled Answers', shuffledAnswers)
+
+	const handleClick = () => {
+		handleChoice(character)
+	}
 	return (
 		<div>
-			<div>Solution : {randomCharacter.name}</div>
-			<div>
-				{shuffledAnswers.map((answer) => (
+			<div onClick={handleClick}>
+				{character.name}
+				{/* {shuffledAnswers.map((answer) => (
 					<div key={answer.id}>{answer.name}</div>
-				))}
+				))} */}
 			</div>
 		</div>
 	)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Children, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Answers from './Answers'
 import Photo from './Photo'
 
@@ -6,6 +6,7 @@ const Home = ({ characters }) => {
 	const [randomCharacter, setRandomCharacter] = useState({})
 	const [turns, setTurns] = useState(0)
 	const [shuffleAnswers, setShuffleAnswers] = useState([])
+	const [choice, setChoice] = useState(null)
 
 	const character = characters.map((character) => ({
 		id: character.character.mal_id,
@@ -17,15 +18,34 @@ const Home = ({ characters }) => {
 		if (character.length !== 0) {
 			const randomNumber = Math.floor(Math.random() * character.length)
 			setRandomCharacter(character[randomNumber])
-
 			const shuffled = [...character].sort(() => 0.5 - Math.random())
 			const shuffled1 = shuffled.slice(0, 3)
-
-			console.log(shuffled1)
-			console.log(character[randomNumber])
-			setShuffleAnswers(shuffled1)
+			const check = shuffled1.some((e) => e.id === character[randomNumber].id)
+			console.log('Check', check)
+			if (check) {
+				findCharacter()
+			} else {
+				const shuffledAnswers = shuffled1
+					.concat(character[randomNumber])
+					.sort(() => Math.random() - 0.5)
+					.map((answer) => ({ ...answer }))
+				console.log('GG Shuffled Answers', shuffledAnswers)
+				setShuffleAnswers(shuffledAnswers)
+			}
+			// console.log('Shuffled1', shuffled1)
+			// console.log(character[randomNumber])
 		}
 	}
+
+	// handle choice
+	const handleChoice = (character) => {
+		setChoice(character)
+	}
+
+	// compare the choice with the answer
+	useEffect(() => {
+		console.log(choice)
+	}, [choice])
 
 	// start a new game automatically
 	useEffect(() => {
@@ -39,12 +59,21 @@ const Home = ({ characters }) => {
 			<div>
 				<button onClick={findCharacter}>New Game</button>
 				{randomCharacter && <Photo randomCharacter={randomCharacter} />}
-				{randomCharacter && (
-					<Answers
+				{
+					randomCharacter &&
+						shuffleAnswers.map((character) => (
+							<Answers
+								key={character.id}
+								character={character}
+								handleChoice={handleChoice}
+								randomCharacter={randomCharacter}
+							/>
+						))
+					/* <Answers
 						shuffleAnswers={shuffleAnswers}
 						randomCharacter={randomCharacter}
-					/>
-				)}
+					/> */
+				}
 				<p>Turns - {turns}</p>
 			</div>
 		</div>
